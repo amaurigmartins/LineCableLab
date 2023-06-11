3b9e0f810116e8bb0a76dd12e27d6bb0950bcea4
Amauri Martins <amaurigmartins@gmail.com>
2023-06-11 18:12:08 -0300

    Delete simu_pipeline directory

--- simu_pipeline/mode_decomp_funs/calc_poles_zeros.m
+++
@@ -1,9 +0,0 @@
-function [pol_co]=calc_poles_zeros(ord,fit)
-
-for k=1:ord
-    for o=1:ord
-        [pol_co(k,o).b,pol_co(k,o).a]=residue(fit(k,o).C,fit(k,o).A,fit(k,o).D);
-        pol_co(k,o).poles=roots(pol_co(k,o).a);
-        pol_co(k,o).zeros=roots(pol_co(k,o).b);
-    end
-end
