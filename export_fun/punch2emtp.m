a1eae8b4e43f6514a392dedaff5dd9a44f2153e9
Amauri Martins <amaurigmartins@gmail.com>
2023-06-16 04:33:34 -0300

    Minor adjustments

--- export_fun/punch2emtp.m
+++ export_fun/punch2emtp.m
@@ -1,13 +1,5 @@
-% close all
-% clear all
-% clc
-% fload='wise_cg.mat';
-% load(fload)
-% % 
-% line_length = 100;
-
-Z=eval(Zmod_src);
-Y=eval(Ymod_src);
+Z=eval([jobid '_data.Z']);
+Y=eval([jobid '_data.Y']);
 
 if size(f,1) ~= 1
 f=f';
