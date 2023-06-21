74a62ff8679f5c235a0d45453c62d85839be6274
Amauri Martins <amaurigmartins@gmail.com>
2023-06-21 15:47:53 -0300

    Add files via upload

--- Laplace_sim_model_update/src_enrg.m
+++ Laplace_sim_model_update/src_enrg.m
@@ -1,11 +1,12 @@
 % Energization
-function [vo1,Vo1]=src_enrg(amp,t_cl,data_t_sim,e)
+function [vo1,Vo1,c]=src_enrg(amp,t_cl,data_t_sim,e)
 
+c=0; % for FFT
 f=50; % Frequency
-o=amp*(cos(2*pi*f*data_t_sim+pi)); % Το συνημίτονο
+o=amp*(cos(2*pi*f*data_t_sim+pi)); % cosine
 samp=max(size(o)); % Size of samples
 
-vo1=[zeros(1,int32(t_cl*e)),o(int32(t_cl*e+1):samp)]; % Το "αποκομμένο" συνημίτονο - int32: Signed 32-bit integer from -2,147,483,648 to 2,147,483,647
+vo1=[zeros(1,int32(t_cl*e)),o(int32(t_cl*e+1):samp)]; % "Clipped" cosine - int32: Signed 32-bit integer from -2,147,483,648 to 2,147,483,647
 
-Vo1=fft(vo1); % FFT "αποκομμένου" συνημιτόνου (με το είδωλο)
-Vo1=Vo1(1,1:(samp/2+1)); % FFT "αποκομμένου" συνημιτόνου (χωρίς το είδωλο)
+Vo1=fft(vo1); % FFT of the "clipped" cosine (with image)
+Vo1=Vo1(1,1:(samp/2+1)); % FFT of the "clipped" cosine (without image)
--- Laplace_sim_model_update/src_sin.m
+++ Laplace_sim_model_update/src_sin.m
@@ -1,9 +1,10 @@
 % Sinusoidal Voltage
-function [vo1,Vo1]=src_sin(amp,data_t_sim)
+function [vo1,Vo1,c]=src_sin(amp,data_t_sim)
 
+c=0; % for FFT
 f=50; % Frequency
-vo1=amp*(cos(2*pi*f*data_t_sim)); % Το συνημίτονο
+vo1=amp*(cos(2*pi*f*data_t_sim)); % cosine
 samp=max(size(data_t_sim)); % Size of samples
 
-Vo1=fft(vo1); % FFT συνημιτόνου (με το είδωλο)
-Vo1=Vo1(1,1:(samp/2+1)); % FFT συνημιτόνου (χωρίς το είδωλο)
+Vo1=fft(vo1); % FFT cosine (with image)
+Vo1=Vo1(1,1:(samp/2+1)); % FFT cosine (without image)
