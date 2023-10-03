3c3792adb94807c4dd5b813290945c2ed32f7034
Amauri Martins <amaurigmartins@gmail.com>
2023-10-03 14:35:07 -0300

    Add files via upload

--- ZY_OHTL_pul_funs/Y_clc_fun.m
+++ ZY_OHTL_pul_funs/Y_clc_fun.m
@@ -1,4 +1,4 @@
-function [Ytot_Imag,Ytot_Pet,Ytot_Wise,Ytot_Papad,Ytot_OvUnd,sigma_g_total,erg_total,Nph] = Y_clc_fun(f_total,ord,ZYprnt,FD_flag,siz,soil,h,d,Geom,ZYsave,jobid)
+function [Ytot_Imag,Ytot_Pet,Ytot_Wise,Ytot_Papad,Ytot_OvUnd,Ytot_Kik,Ytot_Sunde2La,sigma_g_total,erg_total,Nph] = Y_clc_fun(f_total,ord,ZYprnt,FD_flag,siz,soil,h,d,Geom,ZYsave,jobid)
 % Variables
 e0=8.854187817e-12;  % Farads/meters
 omega_total=2*pi*f_total;
@@ -20,40 +20,55 @@ for k=1:1:ord
 end
 
 %% Earth data
-% Earth Electric Parameters
-erg_total=soil.erg;         %relative permittivity of earth
-m_g=soil.m_g;     %permeability of earth
-sigma_g_total=soil.sigma_g; %conductivity of earth
 er_HF_LS=5;
 er_HF_M=8;
 er_HF_dry=3.5;
 
-rho_LF=1/sigma_g_total;
-
-% FD soil parameters
-if FD_flag == 0
-    sigma_g_total=sigma_g_total.*ones(siz,1);
-    erg_total=erg_total.*ones(siz,1);
-elseif FD_flag == 1
-    [sigma_g_total,rho_g,erg_total] = soilFD_LongSmith_fun(rho_LF,er_HF_LS,f_total);
-elseif FD_flag == 2
-    [sigma_g_total,rho_g,erg_total] = soilFD_Portela_fun(rho_LF,f_total); % Portela
-elseif FD_flag == 3
-    [sigma_g_total,rho_g,erg_total] = soilFD_AlVis_fun(rho_LF,f_total); % Alipio & Visacro
-elseif FD_flag == 4
-    [sigma_g_total,rho_g,erg_total] = soilFD_DM_fun(rho_LF,er_HF_dry,f_total); % Datsios & Mikropoulos
-elseif FD_flag == 5
-    [sigma_g_total,rho_g,erg_total] = soilFD_Scott_fun(rho_LF,f_total); % Scott
-elseif FD_flag == 6
-    [sigma_g_total,rho_g,erg_total] = soilFD_M_fun(rho_LF,er_HF_M,f_total); % Messier
-elseif FD_flag == 7
-    [sigma_g_total,rho_g,erg_total] = soilFD_VisPor_fun(rho_LF,f_total); % Visacro & Portela
-elseif FD_flag == 8
-    [sigma_g_total,rho_g,erg_total] = soilFD_VisAl_fun(rho_LF,f_total); % Visacro & Alipio
-elseif FD_flag == 9
-    [sigma_g_total,rho_g,erg_total] = soilFD_Cigre_fun(rho_LF,f_total); % Cigre
+num_layers=1;
+if isfield(soil,'layer')
+    num_layers=num_layers+length(soil.layer);
 end
-e_g_total=e0.*erg_total;
+
+for l=1:num_layers
+    % Earth Electric Parameters
+    if l==num_layers
+        epsr(:,l)=soil.erg;         %relative permittivity of earth
+        m_g(:,l)=soil.m_g;     %permeability of earth
+        sig(:,l)=soil.sigma_g; %conductivity of earth
+    elseif l < num_layers
+        epsr(:,l)=soil.layer(l).erg;         %relative permittivity of earth
+        m_g(:,l)=soil.layer(l).m_g;     %permeability of earth
+        sig(:,l)=soil.layer(l).sigma_g; %conductivity of earth
+    end
+
+    rho_LF(:,l)=1./sig(:,l);
+
+    % FD soil parameters
+    if FD_flag == 0
+        sigma_g_total(:,l)=sig(:,l).*ones(siz,1);
+        erg_total(:,l)=epsr(:,l).*ones(siz,1);
+    elseif FD_flag == 1
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_LongSmith_fun(rho_LF(:,l),er_HF_LS,f_total);
+    elseif FD_flag == 2
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Portela_fun(rho_LF(:,l),f_total); % Portela
+    elseif FD_flag == 3
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_AlVis_fun(rho_LF(:,l),f_total); % Alipio & Visacro
+    elseif FD_flag == 4
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_DM_fun(rho_LF(:,l),er_HF_dry,f_total); % Datsios & Mikropoulos
+    elseif FD_flag == 5
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Scott_fun(rho_LF(:,l),f_total); % Scott
+    elseif FD_flag == 6
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_M_fun(rho_LF(:,l),er_HF_M,f_total); % Messier
+    elseif FD_flag == 7
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_VisPor_fun(rho_LF(:,l),f_total); % Visacro & Portela
+    elseif FD_flag == 8
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_VisAl_fun(rho_LF(:,l),f_total); % Visacro & Alipio
+    elseif FD_flag == 9
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Cigre_fun(rho_LF(:,l),f_total); % Cigre
+    end
+    e_g_total(:,l)=e0.*erg_total(:,l);
+
+end
 
 %% Calculations
 % Dynamic allocation
@@ -64,14 +79,17 @@ Ptot_Papad=zeros(Nph,Nph,siz);
 Ptot_OvUnd=zeros(Nph,Nph,siz);
 Ytot_Papad=zeros(Nph,Nph,siz);
 Ytot_OvUnd=zeros(Nph,Nph,siz);
+Ytot_Kik=zeros(Nph,Nph,siz);
+Ytot_Sunde2La=zeros(Nph,Nph,siz);
 Pin_test=zeros(ord,ord,siz);
 
 %% Shunt admittance
 for k=1:siz
     omega=omega_total(k);
     f=f_total(k);
-    erg=erg_total(k);
+%     erg=erg_total(k);
     sigma_g=sigma_g_total(k);
+    mu_g=m_g(end);
     e_g=e_g_total(k);
     n1_tetragwno=1;
     
@@ -87,65 +105,93 @@ for k=1:siz
     %Potential Coeficients of Self Admittance - Perfect Ground
     Ps_pet_perf=P_pet_slf_perf(h,cab_ex,ord);
     %Potential Coeficients of Self Admittance - Imperfect Ground
-    Ps_pet_imperf=P_pet_slf_imperf(h,e_g,m_g,sigma_g,omega,ord);
+    Ps_pet_imperf=P_pet_slf_imperf(h,e_g,mu_g,sigma_g,omega,ord);
     % Total self - imperfect
     Ps_pet=Ps_pet_perf+Ps_pet_imperf;
     % Total self - perfect
     Ps_imag=Ps_pet_perf;
-    
+
     % Wise's formula
-    Ps_wise_imperf=P_wise_slf_imperf(h,e_g,m_g,sigma_g,omega,ord);
+    Ps_wise_imperf=P_wise_slf_imperf(h,e_g,mu_g,sigma_g,omega,ord);
     Ps_wise=Ps_pet_perf+Ps_wise_imperf;
+
     
+
     %% Potential coefficients (mutual terms)
     %Potential Coeficients of Mutal Admittance - Perfect Ground
     Pm_pet_perf=P_pet_mut_perf(h,d,ord);
     %Potential Coeficients of Mutal Admittance - Imperfect Ground
-    Pm_pet_imperf=P_pet_mut_imperf(h,d,e_g,m_g,sigma_g,omega,ord);
+    Pm_pet_imperf=P_pet_mut_imperf(h,d,e_g,mu_g,sigma_g,omega,ord);
     % Total Mutual - imperfect
     Pm_pet=Pm_pet_perf+Pm_pet_imperf;
     % Total Mutual - imperfect
     Pm_imag=Pm_pet_perf;
     
     % Wise's formula
-    Pm_wise_imperf=P_wise_mut_imperf(h,d,e_g,m_g,sigma_g,omega,ord);
+    Pm_wise_imperf=P_wise_mut_imperf(h,d,e_g,mu_g,sigma_g,omega,ord);
     Pm_wise=Pm_pet_perf+Pm_wise_imperf;
-    
+
+    % Kikuchi's formula - Self and mutual
+    global kxa;if isempty(kxa);kxa='k0';end;
+    Ps_kik=P_kik_slf(h,cab_ex,e_g ,mu_g,sigma_g,f,ord,kxa); % self coefficients of the overhead conductors
+    Pm_kik=P_kik_mut(h,d,e_g,mu_g,sigma_g,f,ord,kxa); % mutual coefficients of the overhead conductors
+
+    if num_layers==2
+        % Sunde's formula for 2-layers
+        sigma_g_la=sigma_g_total(1,:);
+        e_g_la=e_g_total(1,:);
+        global kxa;if isempty(kxa);kxa='k0';end;
+        t=-soil.layer(1).t;
+        Ps2la=P_ohl_slf_2lay(h,cab_ex,e_g_la,m_g,sigma_g_la,t,f,ord,kxa);
+        Pm2la=P_ohl_mut_2lay(h,d,e_g_la,m_g,sigma_g_la,t,f,ord,kxa);
+    end
    
     
     %% Total Pottential Coefficient Matrix
     L_Q_imag_mat=Ps_imag+Pm_imag+Pin_mat;
     L_Q_mat=Ps_pet+Pm_pet+Pin_mat;
     L_Q_wise=Ps_wise+Pm_wise+Pin_mat;
+    L_Q_kik=Ps_kik+Pm_kik+(Pin_mat./(2*pi*e0));
+    L_Q_Sunde2La=Ps2la+Pm2la+(Pin_mat./(2*pi*e0));
     
     % Bundle reduction
     L_Q_imag_mat = bundleReduction(ph_order,L_Q_imag_mat);
     L_Q_mat = bundleReduction(ph_order,L_Q_mat);
     L_Q_wise = bundleReduction(ph_order,L_Q_wise);
+    L_Q_kik = bundleReduction(ph_order,L_Q_kik);
+    L_Q_Sunde2La = bundleReduction(ph_order,L_Q_Sunde2La);
     
     % Total Admittance Matrices
     Ytot_Imag(:,:,k)=1i.*omega.*e0.*2.*pi.*n1_tetragwno.*inv(L_Q_imag_mat);
     Ytot_Pet(:,:,k)=1i.*omega.*e0.*2.*pi.*n1_tetragwno.*inv(L_Q_mat);
     Ytot_Wise(:,:,k)=1i.*omega.*e0.*2.*pi.*n1_tetragwno.*inv(L_Q_wise);
+    Ytot_Kik(:,:,k)=1i.*omega.*inv(L_Q_kik);
+    Ytot_Sunde2La(:,:,k)=1i.*omega.*inv(L_Q_Sunde2La);
 
     elseif all(h<0) % all conductors are underground
-        kx='k1'; %set 'k0' (string) to air layer, 'k1' for earth, 0 to neglect
-        Ps_papad=P_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx);
-        Pm_papad=P_papad_mut(h,d,e_g,m_g,sigma_g,f,ord,kx);
+        global kxe;if isempty(kxe);kxe='k1';end;
+        Ps_papad=P_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxe);
+        Pm_papad=P_papad_mut(h,d,e_g,m_g,sigma_g,f,ord,kxe);
         Ptot_Papad(:,:,k)=Ps_papad+Pm_papad+(Pin_mat./(2*pi*e0));
         Ptot_Papad(:,:,k) = bundleReduction(ph_order,Ptot_Papad(:,:,k));
 
         Ytot_Papad(:,:,k)=1i.*omega.*inv(Ptot_Papad(:,:,k));
     
     else %over-under
-        kx='k0'; %set 'k0' (string) to air layer, 'k1' for earth, 0 to neglect
-
-        Ps_papad=P_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx); % self coefficients of the underground conductors
-        Pm_papad=P_papad_mut(h,d,e_g ,m_g,sigma_g,f,ord,kx); % mutual coefficients of the underground conductors
-        Ps_kik=P_kik_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx); % self coefficients of the overhead conductors
-        Pm_kik=P_kik_mut(h,d,e_g ,m_g,sigma_g,f,ord,kx); % mutual coefficients of the overhead conductors
-        Pm_new=P_new_mut(h,d,e_g ,m_g,sigma_g,f,ord,0); % mutual coefficients in the mixed configuration
-
+        global kxe;if isempty(kxe);kxe=0;end;
+        %set 'k0' (string) to air layer, 'k1' for earth, 0 to neglect
+        Ps_papad=P_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxe); % self coefficients of the underground conductors
+        Pm_papad=P_papad_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxe); % mutual coefficients of the underground conductors
+        global kxa;if isempty(kxa);kxa=0;end;
+        Ps_kik=P_kik_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxa); % self coefficients of the overhead conductors
+        Pm_kik=P_kik_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxa); % mutual coefficients of the overhead conductors
+        global kxm;if isempty(kxm);kxm=0;end;
+        global Pm_flag;if isempty(Pm_flag);Pm_flag=true;end;
+        if Pm_flag
+            Pm_new=P_new_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxm); % mutual coefficients in the mixed configuration
+        else
+            Pm_new=zeros(ord,ord);
+        end
         Ptot_OvUnd(:,:,k)=Ps_papad+Pm_papad+Ps_kik+Pm_kik+Pm_new+(Pin_mat./(2*pi*e0));
         Ptot_OvUnd(:,:,k) = bundleReduction(ph_order,Ptot_OvUnd(:,:,k));
 
@@ -156,10 +202,10 @@ for k=1:siz
 end
 
 if (ZYprnt)
-    plotY_fun_ct(f_total,Nph,Ytot_Imag,Ytot_Pet,Ytot_Wise,Ytot_Papad,Ytot_OvUnd,jobid)
+    plotY_fun_ct(f_total,Nph,Ytot_Imag,Ytot_Pet,Ytot_Wise,Ytot_Papad,Ytot_OvUnd,Ytot_Kik,Ytot_Sunde2La,jobid)
 end
 
 if (ZYsave)
 fname = fullfile(pwd, 'Y_pul_output.mat');
-save(fname,'Ytot_Pet', 'Ytot_Imag', 'Ytot_Wise', 'f', 'Ytot_Papad', 'Ytot_OvUnd');
+save(fname,'Ytot_Pet', 'Ytot_Imag', 'Ytot_Wise', 'f', 'Ytot_Papad', 'Ytot_OvUnd', 'Ytot_Kik','Ytot_Sunde2La');
 end
--- ZY_OHTL_pul_funs/Z_clc_fun.m
+++ ZY_OHTL_pul_funs/Z_clc_fun.m
@@ -1,7 +1,7 @@
-function [Ztot_Carson,Ztot_Noda,Ztot_Deri,Ztot_AlDe,Ztot_Sunde,Ztot_Pettersson,Ztot_Semlyen,Ztot_Wise,Ztot_Papad,Ztot_OvUnd,Nph] = Z_clc_fun(f_total,ord,ZYprnt,FD_flag,siz,soil,h,d,Geom,ZYsave,jobid)
+function [Ztot_Carson,Ztot_Noda,Ztot_Deri,Ztot_AlDe,Ztot_Sunde,Ztot_Pettersson,Ztot_Semlyen,Ztot_Wise,Ztot_Papad,Ztot_OvUnd,Ztot_Kik,Ztot_Sunde2La,Nph] = Z_clc_fun(f_total,ord,ZYprnt,FD_flag,siz,soil,h,d,Geom,ZYsave,jobid)
 %% Variables
 e0=8.854187817e-12;  % Farads/meters
-m0=4*pi*1e-7;        % Henry's/meters
+% m0=4*pi*1e-7;        % Henry's/meters
 
 omega_total=2*pi*f_total;
 
@@ -26,40 +26,56 @@ rad_ex=Geom(:,5); % external radius of conductor
 sigma_w=1./Geom(:,6); %conductivity of conductor
 mrw=Geom(:,7); % relative permeability of conductor
 %% Earth data
-% Earth Electric Parameters
-erg_total=soil.erg;         %relative permittivity of earth
-m_g=soil.m_g;     %permeability of earth
-sigma_g_total=soil.sigma_g; %conductivity of earth
 er_HF_LS=5;
 er_HF_M=8;
 er_HF_dry=3.5;
 
-rho_LF=1/sigma_g_total;
-
-% FD soil parameters
-if FD_flag == 0
-    sigma_g_total=sigma_g_total.*ones(siz,1);
-    erg_total=erg_total.*ones(siz,1);
-elseif FD_flag == 1
-    [sigma_g_total,rho_g,erg_total] = soilFD_LongSmith_fun(rho_LF,er_HF_LS,f_total);
-elseif FD_flag == 2
-    [sigma_g_total,rho_g,erg_total] = soilFD_Portela_fun(rho_LF,f_total); % Portela
-elseif FD_flag == 3
-    [sigma_g_total,rho_g,erg_total] = soilFD_AlVis_fun(rho_LF,f_total); % Alipio & Visacro
-elseif FD_flag == 4
-    [sigma_g_total,rho_g,erg_total] = soilFD_DM_fun(rho_LF,er_HF_dry,f_total); % Datsios & Mikropoulos
-elseif FD_flag == 5
-    [sigma_g_total,rho_g,erg_total] = soilFD_Scott_fun(rho_LF,f_total); % Scott
-elseif FD_flag == 6
-    [sigma_g_total,rho_g,erg_total] = soilFD_M_fun(rho_LF,er_HF_M,f_total); % Messier
-elseif FD_flag == 7
-    [sigma_g_total,rho_g,erg_total] = soilFD_VisPor_fun(rho_LF,f_total); % Visacro & Portela
-elseif FD_flag == 8
-    [sigma_g_total,rho_g,erg_total] = soilFD_VisAl_fun(rho_LF,f_total); % Visacro & Alipio
-elseif FD_flag == 9
-    [sigma_g_total,rho_g,erg_total] = soilFD_Cigre_fun(rho_LF,f_total); % Cigre
+num_layers=1;
+if isfield(soil,'layer')
+    num_layers=num_layers+length(soil.layer);
 end
-e_g_total=e0.*erg_total;
+
+for l=1:num_layers
+    % Earth Electric Parameters
+    if l==num_layers
+        epsr(:,l)=soil.erg;         %relative permittivity of earth
+        m_g(:,l)=soil.m_g;     %permeability of earth
+        sig(:,l)=soil.sigma_g; %conductivity of earth
+    elseif l < num_layers
+        epsr(:,l)=soil.layer(l).erg;         %relative permittivity of earth
+        m_g(:,l)=soil.layer(l).m_g;     %permeability of earth
+        sig(:,l)=soil.layer(l).sigma_g; %conductivity of earth
+    end
+
+    rho_LF(:,l)=1./sig(:,l);
+
+    % FD soil parameters
+    if FD_flag == 0
+        sigma_g_total(:,l)=sig(:,l).*ones(siz,1);
+        erg_total(:,l)=epsr(:,l).*ones(siz,1);
+    elseif FD_flag == 1
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_LongSmith_fun(rho_LF(:,l),er_HF_LS,f_total);
+    elseif FD_flag == 2
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Portela_fun(rho_LF(:,l),f_total); % Portela
+    elseif FD_flag == 3
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_AlVis_fun(rho_LF(:,l),f_total); % Alipio & Visacro
+    elseif FD_flag == 4
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_DM_fun(rho_LF(:,l),er_HF_dry,f_total); % Datsios & Mikropoulos
+    elseif FD_flag == 5
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Scott_fun(rho_LF(:,l),f_total); % Scott
+    elseif FD_flag == 6
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_M_fun(rho_LF(:,l),er_HF_M,f_total); % Messier
+    elseif FD_flag == 7
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_VisPor_fun(rho_LF(:,l),f_total); % Visacro & Portela
+    elseif FD_flag == 8
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_VisAl_fun(rho_LF(:,l),f_total); % Visacro & Alipio
+    elseif FD_flag == 9
+        [sigma_g_total(:,l),rho_g(:,l),erg_total(:,l)] = soilFD_Cigre_fun(rho_LF(:,l),f_total); % Cigre
+    end
+    e_g_total(:,l)=e0.*erg_total(:,l);
+
+end
+
 %% Calculations
 % Dynamic allocation
 Ztot_Carson=zeros(Nph,Nph,siz);
@@ -72,6 +88,8 @@ Ztot_Semlyen=zeros(Nph,Nph,siz);
 Ztot_AlDe=zeros(Nph,Nph,siz);
 Ztot_Papad=zeros(Nph,Nph,siz);
 Ztot_OvUnd=zeros(Nph,Nph,siz);
+Ztot_Kik=zeros(Nph,Nph,siz);
+Ztot_Sunde2La=zeros(Nph,Nph,siz);
 
 Zin_test=zeros(ord,ord,siz);
 Zg_Carson_mat=zeros(ord,ord,siz);
@@ -80,8 +98,9 @@ Zg_Carson_mat=zeros(ord,ord,siz);
 for k=1:siz
     omega=omega_total(k);
     f=f_total(k);
-    erg=erg_total(k);
+%     erg=erg_total(k);
     sigma_g=sigma_g_total(k);
+    mu_g=m_g(end);
     e_g=e_g_total(k);
     %freq=f(k);
 
@@ -107,40 +126,56 @@ for k=1:siz
 
         %%%% Deri's Method %%%%
         % Self Impedances
-        Zs_deri=Z_der_slf(h,m_g,sigma_g,omega,ord);
+        Zs_deri=Z_der_slf(h,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_deri=Z_der_mut(h,d,m_g,sigma_g,omega,ord);
+        Zm_deri=Z_der_mut(h,d,mu_g,sigma_g,omega,ord);
 
         %%%% Alvarado - Betancourt Method %%%%
         % Self Impedances
-        Zs_alde=Z_alvadetan_slf(h,e_g,m_g,sigma_g,omega,ord);
+        Zs_alde=Z_alvadetan_slf(h,e_g,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_alde=Z_alvadetan_mut(h,d,e_g,m_g,sigma_g,omega,ord);
+        Zm_alde=Z_alvadetan_mut(h,d,e_g,mu_g,sigma_g,omega,ord);
 
         %%%% Sunde's Method %%%%
         % Self Impedances
-        Zs_sunde=Z_snd_slf(h,e_g,m_g,sigma_g,omega,ord);
+        Zs_sunde=Z_snd_slf(h,e_g,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_sunde=Z_snd_mut(h,d,e_g,m_g,sigma_g,omega,ord);
+        Zm_sunde=Z_snd_mut(h,d,e_g,mu_g,sigma_g,omega,ord);
 
         %%%% Semlyen's Method %%%%
         % Self Impedances
-        Zs_semlyen=Z_sln_slf(h,e_g,m_g,sigma_g,omega,ord);
+        Zs_semlyen=Z_sln_slf(h,e_g,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_semlyen=Z_sln_mut(h,d,e_g,m_g,sigma_g,omega,ord);
+        Zm_semlyen=Z_sln_mut(h,d,e_g,mu_g,sigma_g,omega,ord);
 
         %%%% Pettersson's Method %%%%
         % Self Impedances
-        Zs_pet=Z_pet_slf(h,e_g,m_g,sigma_g,omega,ord);
+        Zs_pet=Z_pet_slf(h,e_g,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_pet=Z_pet_mut(h,d,e_g,m_g,sigma_g,omega,ord);
+        Zm_pet=Z_pet_mut(h,d,e_g,mu_g,sigma_g,omega,ord);
 
         %%%% Wise's Integral Formula %%%%
         % Self Impedances
-        Zs_wise=Z_wise_slf(h,e_g,m_g,sigma_g,omega,ord);
+        Zs_wise=Z_wise_slf(h,e_g,mu_g,sigma_g,omega,ord);
         % Mutual Impedances
-        Zm_wise=Z_wise_mut(h,d,e_g,m_g,sigma_g,omega,ord);
+        Zm_wise=Z_wise_mut(h,d,e_g,mu_g,sigma_g,omega,ord);
 
+        %%%% Kikuchi's Integral Formula %%%%
+        global kxa;if isempty(kxa);kxa='k0';end;
+        % Self Impedances
+        Zs_kik=Z_kik_slf(h,cab_ex,e_g ,mu_g,sigma_g,f,ord,kxa); % self impedances of the overhead conductors
+        % Mutual Impedances
+        Zm_kik=Z_kik_mut(h,d,e_g ,mu_g,sigma_g,f,ord,kxa); % mutual impedances of the overhead conductors
+
+        if num_layers==2
+            % Sunde's formula for 2-layers
+            sigma_g_la=sigma_g_total(1,:);
+            e_g_la=e_g_total(1,:);
+            global kxa;if isempty(kxa);kxa='k0';end;
+            t=-soil.layer(1).t;
+            Zs2la=Z_ohl_slf_2lay(h,cab_ex,e_g_la,m_g,sigma_g_la,t,f,ord,kxa);
+            Zm2la=Z_ohl_mut_2lay(h,d,e_g_la,m_g,sigma_g_la,t,f,ord,kxa);
+        end
 
         %%%% Total Matrices %%%%
         Zg_Carson=Zs_carson+Zm_carson;
@@ -151,6 +186,8 @@ for k=1:siz
         Zg_Pet=Zs_pet+Zm_pet;
         Zg_Wise=Zs_wise+Zm_wise;
         Zg_Sem=Zs_semlyen+Zm_semlyen;
+        Zg_kik=Zs_kik+Zm_kik;
+        Zg_Sunde2La=Zs2la+Zm2la;
 
         Zpg=Z_self_mut_pg(h,d,cab_ex,omega,ord);        % Influence of perfect earth
 
@@ -164,6 +201,8 @@ for k=1:siz
         Z_Sunde=Zin+Zpg+Zg_Sund;
         Z_Pettersson=Zin+Zpg+Zg_Pet;
         Z_Wise=Zin+Zpg+Zg_Wise;
+        Z_kik=Zin+Zg_kik;
+        Z_Sunde2La=Zin+Zg_Sunde2La;
         Z_Semlyen=Zin+Zpg+Zg_Sem;
 
         % Bundle reduction matrices
@@ -175,12 +214,14 @@ for k=1:siz
         Ztot_Semlyen(:,:,k) = bundleReduction(ph_order,Z_Semlyen);
         Ztot_Sunde(:,:,k) = bundleReduction(ph_order,Z_Sunde);
         Ztot_Wise(:,:,k) = bundleReduction(ph_order,Z_Wise);
+        Ztot_Kik(:,:,k) = bundleReduction(ph_order,Z_kik);
+        Ztot_Sunde2La(:,:,k) = bundleReduction(ph_order,Z_Sunde2La);
 
     elseif all(h<0) % all conductors are underground
-        kx='k0';  %set 'k0' (string) to air layer, 'k1' for earth, 0 to neglect
+        global kxe;if isempty(kxe);kxe='k1';end;
                
-        Zs_papad=Z_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx);
-        Zm_papad=Z_papad_mut(h,d,e_g,m_g,sigma_g,f,ord,kx);
+        Zs_papad=Z_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxe);
+        Zm_papad=Z_papad_mut(h,d,e_g,m_g,sigma_g,f,ord,kxe);
         Ztot_Papad(:,:,k)=Zs_papad+Zm_papad;
 
         Ztot_Papad(:,:,k)=Zin_test(:,:,k)+Ztot_Papad(:,:,k);
@@ -188,12 +229,14 @@ for k=1:siz
     
     else % a mixed overhead-underground setup. Need to choose carefully the self impedance formula
         
-        kx='k0';
-        Zs_papad=Z_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx); % self impedances of the underground conductors
-        Zm_papad=Z_papad_mut(h,d,e_g ,m_g,sigma_g,f,ord,kx); % mutual impedances of the underground conductors
-        Zs_kik=Z_kik_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kx); % self impedances of the overhead conductors
-        Zm_kik=Z_kik_mut(h,d,e_g ,m_g,sigma_g,f,ord,kx); % mutual impedances of the overhead conductors
-        Zm_new=Z_new_mut(h,d,e_g ,m_g,sigma_g,f,ord,0); % mutual impedances in the mixed configuration
+        global kxe;if isempty(kxe);kxe=0;end;
+        Zs_papad=Z_papad_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxe); % self impedances of the underground conductors
+        Zm_papad=Z_papad_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxe); % mutual impedances of the underground conductors
+        global kxa;if isempty(kxa);kxa=0;end;
+        Zs_kik=Z_kik_slf(h,cab_ex,e_g ,m_g,sigma_g,f,ord,kxa); % self impedances of the overhead conductors
+        Zm_kik=Z_kik_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxa); % mutual impedances of the overhead conductors
+        global kxm;if isempty(kxm);kxm=0;end;
+        Zm_new=Z_new_mut(h,d,e_g ,m_g,sigma_g,f,ord,kxm); % mutual impedances in the mixed configuration
         Ztot_OvUnd(:,:,k)=Zs_papad+Zm_papad+Zs_kik+Zm_kik+Zm_new;
         Ztot_OvUnd(:,:,k)=Zin_test(:,:,k)+Ztot_OvUnd(:,:,k);
         Ztot_OvUnd(:,:,k) = bundleReduction(ph_order,Ztot_OvUnd(:,:,k));
@@ -205,10 +248,10 @@ for k=1:siz
 end
 %% Plot parameters
 if (ZYprnt)
-    plotZ_fun_ct(f_total,Nph,Ztot_Carson,Ztot_Noda,Ztot_Deri,Ztot_AlDe,Ztot_Sunde,Ztot_Pettersson,Ztot_Semlyen,Ztot_Wise,Ztot_Papad,Ztot_OvUnd,jobid);
+    plotZ_fun_ct(f_total,Nph,Ztot_Carson,Ztot_Noda,Ztot_Deri,Ztot_AlDe,Ztot_Sunde,Ztot_Pettersson,Ztot_Semlyen,Ztot_Wise,Ztot_Papad,Ztot_OvUnd,Ztot_Kik,Ztot_Sunde2La,jobid);
 end
 
 if (ZYsave)
     fname = fullfile(pwd,'Z_pul_output.mat');
-    save(fname,'Ztot_Carson', 'Ztot_Noda', 'Ztot_Deri', 'Ztot_AlDe', 'Ztot_Sunde', 'Ztot_Pettersson', 'Ztot_Semlyen', 'Ztot_Wise','Ztot_Papad','Ztot_OvUnd' );
+    save(fname,'Ztot_Carson', 'Ztot_Noda', 'Ztot_Deri', 'Ztot_AlDe', 'Ztot_Sunde', 'Ztot_Pettersson', 'Ztot_Semlyen', 'Ztot_Wise','Ztot_Papad','Ztot_OvUnd','Ztot_Kik','Ztot_Sunde2La' );
 end
