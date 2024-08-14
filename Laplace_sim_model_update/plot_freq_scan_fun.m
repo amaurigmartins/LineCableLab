dd49033027dfdfec461d2bc979aed38f95834632
Amauri Martins <amaurigmartins@gmail.com>
2024-08-14 12:10:24 +0200

    Add files via upload

--- Laplace_sim_model_update/plot_freq_scan_fun.m
+++ Laplace_sim_model_update/plot_freq_scan_fun.m
@@ -9,7 +9,6 @@ function [] = plot_freq_scan_fun(V,ord,freq,tag)
 
     for o=1:2*ord
         figure('Name', ['FreqScan' num2str(o) '_' tag])
-        figure(o)
         semilogx(freq,Vmag(o,:),'LineWidth',2);
         xlabel('Frequency [Hz]')
         ylabel('Magnitude [pu]')
