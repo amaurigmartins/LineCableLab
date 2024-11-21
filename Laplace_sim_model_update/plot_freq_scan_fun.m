99b50b533f8416224b3ff84a3ac8cb11b335727c
Amauri Martins <amaurigmartins@gmail.com>
2024-11-21 11:03:35 -0300

    Add files via upload

--- Laplace_sim_model_update/plot_freq_scan_fun.m
+++ Laplace_sim_model_update/plot_freq_scan_fun.m
@@ -1,19 +1,19 @@
 function [] = plot_freq_scan_fun(V,ord,freq,tag)
 
-    Vmag=abs(V);
+Vmag=abs(V);
 
-    for i=1:ord
-        plottitle{i}=sprintf('Phase #%d - Sending terminal',i);
-        plottitle{i+ord}=sprintf('Phase #%d - Receiving terminal',i);
-    end
+for i=1:ord
+    plottitle{i}=sprintf('Phase #%d - Sending terminal',i);
+    plottitle{i+ord}=sprintf('Phase #%d - Receiving terminal',i);
+end
 
-    for o=1:2*ord
-        figure('Name', ['FreqScan' num2str(o) '_' tag])
-        semilogx(freq,Vmag(o,:),'LineWidth',2);
-        xlabel('Frequency [Hz]')
-        ylabel('Magnitude [pu]')
-        title(plottitle{o});
-        grid on
-    end
+for o=1:2*ord
+    figure('Name', ['FreqScan' num2str(o) '_' tag])
+    semilogx(freq,Vmag(o,:),'LineWidth',2);
+    xlabel('Frequency [Hz]')
+    ylabel('Magnitude [pu]')
+    title(plottitle{o});
+    grid on
+end
 
 end
